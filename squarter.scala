7ec56fec26b86539ac699d69b612e228f4165bf1
James Laver <james@trustrobots.com>
2013-12-28 23:56:33 +0000

    Renamed WordF -> Word, Word -> NamedWord. NamedWord will be phased out at some point

--- squarter.scala
+++ squarter.scala
@@ -6,34 +6,34 @@ package object forth {
   case class InvalidArgumentException(word: String) extends Exception(word)
   case class UnexpectedThingException(thing: String) extends Exception(thing)
 
-  type WordF = Stacks => Try[Stacks]
+  type Word = Stacks => Try[Stacks]
   type WordM = Try[Stacks] => Try[Stacks]
 
-  case class Word(name: String, value: WordF)
+  case class NamedWord(name: String, value: Word)
 
 /* this one means we don't have to use pure if we don't want to
-  implicit class WordFWrapper(val f:(Stacks => Try[Stacks])) {
-    def >>(f2:WordF) = (s:Stacks) => f2(f(s).get)
+  implicit class WordWrapper(val f:(Stacks => Try[Stacks])) {
+    def >>(f2:Word) = (s:Stacks) => f2(f(s).get)
   }
 */
   implicit class WordMWrapper(val f:Try[Stacks]) {
-    def >>(f2:WordF) = f2(f.get)
+    def >>(f2:Word) = f2(f.get)
   }
   object Machine {
-    def default_words:List[Word] = List(
-      Word("+", words.plus),
-      Word("-", words.minus),
-      Word("*", words.multiply),
-      Word("/", words.divide),
-      Word("inc", words.inc),
-      Word("dec", words.dec),
-      Word("dup", words.dup),
-      Word("drop", words.drop),
-      Word("swap", words.swap),
-      Word("over", words.over),
-      Word("rot", words.rot),
-      Word("rrot", words.rrot),
-      Word("puts", words.puts)
+    def default_words:List[NamedWord] = List(
+      NamedWord("+", words.plus),
+      NamedWord("-", words.minus),
+      NamedWord("*", words.multiply),
+      NamedWord("/", words.divide),
+      NamedWord("inc", words.inc),
+      NamedWord("dec", words.dec),
+      NamedWord("dup", words.dup),
+      NamedWord("drop", words.drop),
+      NamedWord("swap", words.swap),
+      NamedWord("over", words.over),
+      NamedWord("rot", words.rot),
+      NamedWord("rrot", words.rrot),
+      NamedWord("puts", words.puts)
     )
     def test_run(ts: List[Any]) = {
       val inp = Stacks(Nil,ts,default_words)
@@ -44,7 +44,7 @@ package object forth {
       case i: Int => s.pushD(i.toLong)
       case b: Boolean => s.pushD(b)
       case f: Double => s.pushD(f)
-      case Word(n,v) => s.pure >> v
+      case NamedWord(n,v) => s.pure >> v
       case _ => {
         println("Unexpected, t")
         Failure(UnexpectedThingException("t"))
@@ -59,7 +59,7 @@ package object forth {
     }
   }
 
-  case class Stacks(data: List[Any], tokens: List[Any], words: List[Word]) {
+  case class Stacks(data: List[Any], tokens: List[Any], words: List[NamedWord]) {
     def minD(n: Long) = this.data.length >= n
     def minT(n: Long) = this.tokens.length >= n
     def pushD(d: Any):Try[Stacks] = Success(Stacks(d::this.data, this.tokens, this.words))
@@ -88,8 +88,8 @@ package object forth {
       case t::ts => Success((t, Stacks(this.data, ts, this.words)))
       case _ => Failure(StackEmptyException("Stack empty"))
     }
-    def pushW(w: Word):Try[Stacks] = Success(Stacks(this.data, this.tokens, w::this.words))
-    def popW():Try[(Word, Stacks)] = this.words match {
+    def pushW(w: NamedWord):Try[Stacks] = Success(Stacks(this.data, this.tokens, w::this.words))
+    def popW():Try[(NamedWord, Stacks)] = this.words match {
       case w::ws => Success((w, Stacks(this.data, this.tokens, ws)))
       case _ => Failure(StackEmptyException("Stack empty"))
     }
@@ -104,17 +104,17 @@ package object forth {
   }
 
   object lifts {
-    def part1(f:(Any => Try[List[Any]])):WordF = (s: Stacks) => for {
+    def part1(f:(Any => Try[List[Any]])):Word = (s: Stacks) => for {
       (a, s2) <- s.popD
       n <- f(a)
       ret <- s2.pushDN(n.reverse)
     } yield ret
-    def part2(f:((Any, Any) => Try[List[Any]])):WordF = (s: Stacks) => for {
+    def part2(f:((Any, Any) => Try[List[Any]])):Word = (s: Stacks) => for {
       (a1, a2, s2) <- s.popD2
       n <- f(a1,a2)
       ret <- s2.pushDN(n.reverse)
     } yield ret
-    def part3(f:((Any, Any, Any) => Try[List[Any]])):WordF = (s: Stacks) => for {
+    def part3(f:((Any, Any, Any) => Try[List[Any]])):Word = (s: Stacks) => for {
       (a1, a2, a3, s2) <- s.popD3
       n <- f(a1,a2,a3)
       ret <- s2.pushDN(n.reverse)
@@ -123,19 +123,19 @@ package object forth {
 
   def pushD(a:Any) = {s:Stacks => s.pushD(a)}
   object words {
-    def plus:WordF = {s:Stacks => s.pure >> lifts.part2(raw_words.plus)}
-    def minus:WordF = lifts.part2(raw_words.minus)
-    def multiply:WordF = lifts.part2(raw_words.multiply)
-    def divide:WordF = lifts.part2(raw_words.divide)
-    def inc:WordF = {s: Stacks => s.pure >> pushD(1) >> words.plus}
-    def dec:WordF = {s: Stacks => s.pure >> pushD(1) >> words.swap >> words.minus}
-    def dup:WordF = lifts.part1(raw_words.dup)
-    def drop:WordF = lifts.part1(raw_words.drop)
-    def swap:WordF = lifts.part2(raw_words.swap)
-    def over:WordF = lifts.part2(raw_words.over)
-    def rot:WordF = lifts.part3(raw_words.rot)
-    def rrot:WordF = lifts.part3(raw_words.rrot)
-    def puts:WordF = lifts.part1(raw_words.puts)
+    def plus:Word = {s:Stacks => s.pure >> lifts.part2(raw_words.plus)}
+    def minus:Word = lifts.part2(raw_words.minus)
+    def multiply:Word = lifts.part2(raw_words.multiply)
+    def divide:Word = lifts.part2(raw_words.divide)
+    def inc:Word = {s: Stacks => s.pure >> pushD(1) >> words.plus}
+    def dec:Word = {s: Stacks => s.pure >> pushD(1) >> words.swap >> words.minus}
+    def dup:Word = lifts.part1(raw_words.dup)
+    def drop:Word = lifts.part1(raw_words.drop)
+    def swap:Word = lifts.part2(raw_words.swap)
+    def over:Word = lifts.part2(raw_words.over)
+    def rot:Word = lifts.part3(raw_words.rot)
+    def rrot:Word = lifts.part3(raw_words.rrot)
+    def puts:Word = lifts.part1(raw_words.puts)
   }
 
   object raw_words {
@@ -214,6 +214,6 @@ package object forth {
 
 object Main {
   def main(args: Array[String]): Unit = {
-    forth.Machine.test_run(List(4,4,forth.Word("+",forth.words.plus),forth.Word("puts",forth.words.puts)))
+    forth.Machine.test_run(List(4,4,forth.NamedWord("+",forth.words.plus),forth.NamedWord("puts",forth.words.puts)))
   }
 }
